1e4b076ac810385e52d62cc2812475df9b6fe66b
nmkmms <illyakoshmak@gmail.com>
2020-02-12 20:40:26 +0200

    fix

--- logic.js
+++ logic.js
@@ -1,19 +1,20 @@
 //Think how to fix bug (buttons on added divs are not working)
+let item_id = 0;
 $(".button").click(function () {
     let item = $(".search").val();
     $(".search").val("");
     if (item.length > 0) {
         $(".bottom-segment").append(
-            '<div class="line">\n' +
+            '<div class="line" id="' + item_id + '">\n' +
             '    <div class="text-block">\n' +
             '        <div class="just-text">' + item + '</div>\n' +
             '    </div>\n' +
             '    <div class="quantity-block">\n' +
-            '        <button class="button-minus">–\n' +
+            '        <button onclick="minus(' + item_id + ')" class="button-minus">–\n' +
             '            <span class="data-tooltip">Зменшити</span>\n' +
             '        </button>\n' +
-            '        <span>&#160;1&#160;</span>\n' +
-            '        <button class="button-plus">+\n' +
+            '        <span class="num">1</span>\n' +
+            '        <button onclick="plus(' + item_id + ')" class="button-plus">+\n' +
             '            <span class="data-tooltip">Збільшити</span>\n' +
             '        </button>\n' +
             '    </div>\n' +
@@ -21,39 +22,56 @@ $(".button").click(function () {
             '        <button class="button-bought">Куплено\n' +
             '            <span class="data-tooltip">Додати до купленого</span>\n' +
             '        </button>\n' +
-            '        <button class="button-delete">x\n' +
+            '        <button onclick="deleteItem(' + item_id +')" class="button-delete">x\n' +
             '            <span class="data-tooltip">Видалити</span>\n' +
             '        </button>\n' +
             '    </div>\n' +
             '</div>'
         );
         $(".stats-bought").append(
-            '<div class="good-to-buy">\n' +
+            '<div class="good-to-buy" id="r' + item_id +'">\n' +
             '    <span class="good-name">' + item + '</span>\n' +
             '    <span class="orange-circle">&#160;1</span>\n' +
             '</div>'
-        )
+        );
+        item_id++;
     }
 });
 
-$(document).ready(function () {
-    $(".button-delete").click(function () {
-        $(this).parent().parent().remove();
-    });
-});
 
-//Add to the next 2 functions ability to change numbers in the right panel
-$(".button-plus").click(function () {
-    let span = $(this).parent().find('.num');
+function deleteItem(id) {
+    let rid = 'r' + id;
+    document.getElementById(id).style.display = 'none';
+    document.getElementById(rid).style.display = 'none';
+}
+
+function plus(id) {
+    let rid = 'r' + id;
+    let span = $("#" + id).find('.num');
     let data = parseInt(span.html());
+
     span.html(data + 1);
+    $('#' + rid).find(".orange-circle").html('&#160;' + (data + 1));
+}
 
-});
-
-$(".button-minus").click(function () {
-    let span = $(this).parent().find('.num');
+function minus(id) {
+    let rid = 'r' + id;
+    let span = $("#" + id).find('.num');
     let data = parseInt(span.html());
+
     if (data != 1) {
         span.html(data - 1);
+        $('#' + rid).find(".orange-circle").html('&#160;' + (data - 1));
     }
-});
+}
+
+function bought(id) {
+    $(".stats-bought").append(
+        '<div class="good-to-buy">\n' +
+        '    <span class="good-name">' + item + '</span>\n' +
+        '    <span class="orange-circle">&#160;1</span>\n' +
+        '</div>'
+    );
+    document.getElementById(id).style.display = 'none';
+    document.getElementById(rid).style.display = 'none';
+}
